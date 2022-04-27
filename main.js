f84f7af8ac0c8ce15ea565f1f79658e1b6b4f15c
MGibson7 <MGibsonASC@gmail.com>
2022-04-27 13:24:51 -0400

    Restart to clean up code

--- main.js
+++ main.js
@@ -28,289 +28,76 @@ let array369 = []
 let array159 = []
 let array357 = []
 
-//WHOSE TURN Initially
+let winner = 'none'
+let turn = 'X'
 
-let turn = 'X';
-
-playerInstructions = document.getElementById("playerInstructions")
-
-let winner = "none"
-
-
-function checkWin(){
-    let result = 0
-    for(char in array123){
-        if (array123[char] == `${turn}`){
-            result += 1;
-        }
-    }
-    if (result == 3){
-        if (turn == "X"){
-            playerInstructions.innerText = "YOU WON!";
-            winner = "user"
-
-        }
-        else{
-            playerInstructions.innerText = "COMPUTER WON :(";
-            winner = "cpu"
-
-        }
-        
-        return winner
-
-    }
-    result = 0;
-    for(char in array456){
-        if (array456[char] == `${turn}`){
-            result += 1;
-        }
-    }
-    if (result == 3){
-        if (turn == "X"){
-            playerInstructions.innerText = "YOU WON!";
-            winner = "user"
-
-        }
-        else{
-            playerInstructions.innerText = "COMPUTER WON :(";
-            winner = "cpu"
-
-        }
-        
-        return winner
-
-    }
-    result = 0;
-    for(char in array789){
-        if (array789[char] == `${turn}`){
-            result += 1;
-        }
-    }
-    if (result == 3){
-        if (turn == "X"){
-            playerInstructions.innerText = "YOU WON!";
-            winner = "user"
-
-        }
-        else{
-            playerInstructions.innerText = "COMPUTER WON :(";
-            winner = "cpu"
-
-        }
-        
-        return winner
-
-    }
-    result = 0;
-    for(char in array147){
-        if (array147[char] == `${turn}`){
-            result += 1;
-        }
-    }
-    if (result == 3){
-        if (turn == "X"){
-            playerInstructions.innerText = "YOU WON!";
-            winner = "user"
-
-        }
-        else{
-            playerInstructions.innerText = "COMPUTER WON :(";
-            winner = "cpu"
-
-        }
-        
-        return winner
-
-    }
-    result = 0;
-    for(char in array159){
-        if (array159[char] == `${turn}`){
-            result += 1;
-        }
-    }
-    if (result == 3){
-        if (turn == "X"){
-            playerInstructions.innerText = "YOU WON!";
-            winner = "user"
-
-        }
-        else{
-            playerInstructions.innerText = "COMPUTER WON :(";
-            winner = "cpu"
-
-        }
-        
-        return winner
-
-    }
-    result = 0;
-    for(char in array258){
-        if (array258[char] == `${turn}`){
-            result += 1;
-        }
-    }
-    if (result == 3){
-        if (turn == "X"){
-            playerInstructions.innerText = "YOU WON!";
-            winner = "user"
-
-        }
-        else{
-            playerInstructions.innerText = "COMPUTER WON :(";
-            winner = "cpu"
-
-        }
-        
-        return winner
-
-    }
-    result = 0;
-    for(char in array357){
-        if (array357[char] == `${turn}`){
-            result += 1;
-        }
-    }
-    if (result == 3){
-        if (turn == "X"){
-            playerInstructions.innerText = "YOU WON!";
-            winner = "user"
-
-        }
-        else{
-            playerInstructions.innerText = "COMPUTER WON :(";
-            winner = "cpu"
-
-        }
-        
-        return winner
-
-    }
-    result = 0;
-    for(char in array369){
-        if (array369[char] == `${turn}`){
-            result += 1;
-        }
-    }
-    if (result == 3){
-        if (turn == "X"){
-            playerInstructions.innerText = "YOU WON!";
-            winner = "user"
-
-        }
-        else{
-            playerInstructions.innerText = "COMPUTER WON :(";
-            winner = "cpu"
-
-        }
-        
-        return winner
-
-    }
-    if (array123.length + array147.length + array159.length + array258.length + array357.length + array369.length + array456.length + array789.length ==24){
-        playerInstructions.innerText = "DRAW"
-        winner = "Draw"
-    }
-    
-}
-
-
-//color: lime;
-//text-shadow: 0 0 10px lime;
-
-let x = 0
-
-
-//DISPLAYING GAME LETTERS
 function gameStart(){
-    for(array in spaceArray){
-        console.log(spaceArray[array])
-        let space = spaceArray[array];
-        space.addEventListener('click', event => {
-            x = space.innerText
-            console.log(x)
-            if (x != 'X' && x != 'O'){
-                if(space.innerText == 1){
-                    array123.push(turn);
-                    array147.push(turn);
-                    array159.push(turn);
-                }
-                else if(space.innerText == 2){
-                    array123.push(turn);
-                    array258.push(turn);
-                }
-                else if(space.innerText == 3){
-                    array123.push(turn);
-                    array357.push(turn);
-                    array369.push(turn);
-                }
-                else if(space.innerText == 4){
-                    array456.push(turn);
-                    array147.push(turn);
-                }
-                else if(space.innerText == 5){
-                    array456.push(turn);
-                    array357.push(turn);
-                    array258.push(turn);
-                    array159.push(turn);
-                }
-                else if(space.innerText == 6){
-                    array456.push(turn);
-                    array369.push(turn);
-                }
-                else if(space.innerText == 7){
-                    array789.push(turn);
-                    array147.push(turn);
-                    array357.push(turn);
-                }
-                else if(space.innerText == 8){
-                    array789.push(turn);
-                    array258.push(turn);
-                }
-                else if(space.innerText == 9){
-                    array789.push(turn);
-                    array159.push(turn);
-                    array369.push(turn);
-                }
-                
-                space.innerHTML = `<h2 style = "color:lime;">${turn}</h2>`
-                checkWin()
-                if (winner != "none"){
-                    console.log("WE HAVE A WINNER")
-                    winner = "none"
-                    array123 = []
-                    array456 = []
-                    array789 = []
-                    array147 = []
-                    array258 = []
-                    array369 = []
-                    array159 = []
-                    array357 = []
-                    for(array in spaceArray){
-                        space = spaceArray[array];
-                        space.innerText = `${parseInt(array)+1}`
-            
+        for (array in spaceArray){
+            let space = spaceArray[array];
+            space.addEventListener('click', event=>{
+                let x = space.innerText
+                console.log(x)
+                if (x != 'X' && x != 'O'){
+                    if(space.innerText == 1){
+                        array123.push(turn);
+                        array147.push(turn);
+                        array159.push(turn);
                     }
+                    else if(space.innerText == 2){
+                        array123.push(turn);
+                        array258.push(turn);
+                    }
+                    else if(space.innerText == 3){
+                        array123.push(turn);
+                        array357.push(turn);
+                        array369.push(turn);
+                    }
+                    else if(space.innerText == 4){
+                        array456.push(turn);
+                        array147.push(turn);
+                    }
+                    else if(space.innerText == 5){
+                        array456.push(turn);
+                        array357.push(turn);
+                        array258.push(turn);
+                        array159.push(turn);
+                    }
+                    else if(space.innerText == 6){
+                        array456.push(turn);
+                        array369.push(turn);
+                    }
+                    else if(space.innerText == 7){
+                        array789.push(turn);
+                        array147.push(turn);
+                        array357.push(turn);
+                    }
+                    else if(space.innerText == 8){
+                        array789.push(turn);
+                        array258.push(turn);
+                    }
+                    else if(space.innerText == 9){
+                        array789.push(turn);
+                        array159.push(turn);
+                        array369.push(turn);
+                    }
+                    
+                    space.innerHTML = `<h2 style = "color:lime;">${turn}</h2>`}})
+                }
+                checkWin()
+                if (winner != 'none'){
+                    //DISPLAY WINNER MESSAGE
+                    gameRestart();
                 }
                 else{
-                    if(turn === 'X'){
-                        turn = 'O';
-                    }
-                    else{
-                        turn = 'X';
-                    }
-
+                    computerMove();
+                    checkWin()
+                    if (winner != 'none'){
+                        //DISPLAY WINNER MESSAGE
+                        gameRestart();
                 }
-                
-                
-                //computerMove()
-                //checkWin()
             }
-        })
-    
-        
-    }
+           
 
-}
 
-gameStart()
 
+gameStart();
